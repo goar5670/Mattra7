b7e599e29c2b3c6829498dfe3d8030124f01133b
BL7Awy <mahadelr19@gmail.com>
2021-01-18 03:55:12 +0200

    new tab link

--- main/src/Components/Item.js
+++ main/src/Components/Item.js
@@ -3,15 +3,15 @@ import {Link} from "react-router-dom"
 function Item(props)
 {
     return (
-        <Link to={`/Ad/${props.item.id}`}>
-            <div className="item">
+        <div className="item">
+            <Link to={`/Ad/${props.item.id}`} target='_blank'>
                 <h3 className="item-title"> {props.item.title} </h3>
                 <p className="item-description">{props.item.description} </p>
                 <p> Address: {props.item.address} </p>
-                <p> Governorate: {props.item.governorate}   Number of rooms: {props.item.rooms}   Unit size: {props.size}
+                <p> Governorate: {props.item.governorate}   Number of rooms: {props.item.rooms}   Unit size: {props.item.size}
                    Rent fee: {props.item.price}   Nearby university: {props.item.university}</p>
-            </div>
-        </Link>
+            </Link>
+        </div>
     )
 }
 
